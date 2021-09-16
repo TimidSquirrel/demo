10751f296b20589728226cde0e365c61980a3bc8
TimidSquirrel <m18810242120@163.com>
2021-09-16 17:34:42 +0800

    20210916

---
+++ vite-cli-demo/src/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header">bac</div>
+</template>
+
+<script>
+    export default {
+        name: "Test"
+    }
+</script>
+
+<style scoped>
+.header{
+	height: 300px;
+	background-color: #ccc;
+}
+</style>
---
+++ vite-cli-demo/src/main.js
@@ -0,0 +1,8 @@
+console.log('123')
+import { createApp, h } from "vue";
+import Test from './Test.vue';
+
+
+const app = createApp(Test)
+app.mount('#app')
+
